3de10e730ddcc01157120af3d87eaefdcf91b139
Erik Zimmermann <erik.zimmermann.ez@gmail.com>
2022-02-24 23:01:12 +0100

    Updates main

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include "application/Cipher.h"
 #include "plugins/encryption/BLOCK.h"
 #include "plugins/encryption/CBC_Cipher.h"
-
+#include "application/DDD/EntryRepository.h"
 
 int main() {
-    const Plugins::Database::SQLiteDatabase database("passwords.db");
+    const Plugins::Database::SQLiteDatabase database("../passwords.db");
     DatabaseInterface::DbInterface dbInterface(&database);
+    DDD::Repositories::EntryRepository::createInstance(dbInterface);
 
     const Cipher* c = new CBC_Cipher(BLOCK::Serpent);
+
     return 0;
 }
