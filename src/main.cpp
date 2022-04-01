08811fdd22556348d51d9f6714794e76d5830170
Erik Zimmermann <erik.zimmermann.ez@gmail.com>
2022-04-02 01:09:15 +0200

    Adds mainloop and master password request

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,27 @@
 #include "application/EntryRepository.h"
 #include "application/PasswordEncryption.h"
 #include "application/InstanceManager.h"
+#include "adapters/ui/UiDataHelper.h"
+#include "plugins/ui/CommandLineInterface.h"
 
 int main() {
     const Plugins::Database::SQLiteDatabase database("../passwords.db");
     const Adapters::Database::DatabaseInterface dbInterface(&database);
     const DDD::Repositories::EntryRepository entryRepository(&dbInterface);
+    InstanceManager::addEntryRepositoryPointer(&entryRepository);
 
-    const ValueObjects::PlaintextPassword masterPassword("100%safePassword");
+    Adapters::UI::UiDataHelper uiDataHelper;
+    Plugins::UI::CommandLineInterface cli(uiDataHelper);
+
+    const ValueObjects::PlaintextPassword masterPassword = cli.requestMasterPassword();
+//    if (!masterPasswordVerifier.verifyMasterPassword(masterPassword)) {}
 
     const Cipher* c = new CBC_Cipher(BLOCK::Serpent);
     const DDD::Services::PasswordEncryptor passwordEncryptor(c, masterPassword);
     const DDD::Services::PasswordDecryptor passwordDecryptor(c, masterPassword);
+    InstanceManager::addEncryptionPointers(&passwordEncryptor, &passwordDecryptor);
 
-    InstanceManager::addPointers(&entryRepository, &passwordEncryptor, &passwordDecryptor);
+    cli.mainloop();
 
     return 0;
 }
---
+++ src/plugins/ui/CommandLineInterface.h
@@ -0,0 +1,19 @@
+#ifndef SRC_COMMANDLINEINTERFACE_H
+#define SRC_COMMANDLINEINTERFACE_H
+
+#include "../../adapters/ui/UiDataHelper.h"
+#include "../../application/PlaintextPassword.h"
+
+namespace Plugins::UI {
+    class CommandLineInterface {
+    public:
+        CommandLineInterface(Adapters::UI::UiDataHelper uiDataHelper): uiDataHelper(uiDataHelper) {}
+        int mainloop() const;
+        DDD::ValueObjects::PlaintextPassword requestMasterPassword() const;
+    private:
+        Adapters::UI::UiDataHelper uiDataHelper;
+    };
+
+
+#endif //SRC_COMMANDLINEINTERFACE_H
+} // Plugins::UI
