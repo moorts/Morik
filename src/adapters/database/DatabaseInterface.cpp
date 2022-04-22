04d5e8728f7027e14357bf334eb72ef243cfe382
Erik Zimmermann <erik.zimmermann.ez@gmail.com>
2022-04-22 15:03:03 +0200

    Excludes MasterPasswordHash when searching for Entries by name

--- src/adapters/database/DatabaseInterface.cpp
+++ src/adapters/database/DatabaseInterface.cpp
@@ -14,7 +14,7 @@ DDD::Entities::Entry DatabaseInterface::getEntry(const DDD::ValueObjects::EntryI
 
 std::set<DDD::Entities::Entry> DatabaseInterface::getEntries(const DDD::ValueObjects::EntryName &entryName) const
 {
-    std::string sql = "SELECT * FROM passwords WHERE EntryName LIKE \"%" + entryName.getString() + "%\";";
+    std::string sql = "SELECT * FROM passwords WHERE EntryName LIKE \"%" + entryName.getString() + "%\" AND EntryId <> 0;";
     auto [entrySet, error] = db->executeSql(sql);
     return entrySet;
 }
